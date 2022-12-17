9962c5139e9270caecc02368591578ab27761012
BluBird4 <alhussienrehan@hotmail.com>
2022-12-18 00:44:40 +0200

    Done with showing a result

--- KB/CG.pl
+++ KB/CG.pl
@@ -1,13 +1,12 @@
-%Including our knowledge base
-:- include(’KB3.pl’).
+% Including our knowledge base
+:- include('KB3.pl').
 
-% q: How do I add the current file into a new git repository?
-% a: git init
-% q: How do I add a file to the staging area?
-% a: git add <file>
-% q: How do I commit the staged snapshot?
-% a: git commit -m “<message>”
-% q: How do I see the status of my local repository?
-% a: git status
+goal(S):- result(A1,s0) , S = result(A1,s0). 
 
+%result(right,result(down,s0))
+result(A,s0):- agent_loc(X,Y), grid(Y1,Y2),  X < Y1 , A = down.
+% Set X to X-1 if its less than the alue of Y1 in grid(Y1,Y2) and set A to right
 
+/*
+check how they return the result , its basically result(action , result ... result(action,s0)
+*/
--- KB/KB3.pl
+++ KB/KB3.pl
@@ -1,5 +1,5 @@
 grid(3,3).
 agent_loc(0,1).
-ships_loc([[0,1]]).
+ships_loc([]).
 station(1,1).
 capacity(1).
